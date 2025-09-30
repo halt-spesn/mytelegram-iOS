038969895fd42d2bf9bbb050a7b1c3682e09648d
HALt <102590462+halt-spesn@users.noreply.github.com>
2025-09-30 19:13:05 +0300

    Update BazelLocation.py

--- build-system/Make/BazelLocation.py
+++ build-system/Make/BazelLocation.py
@@ -100,8 +100,7 @@ def locate_bazel(base_path, cache_host_or_path, cache_dir):
     if os.path.isfile(bazel_path) and versions.bazel_version_sha256 is not None:
         test_sha256 = calculate_sha256(bazel_path)
         if test_sha256 != versions.bazel_version_sha256:
-            print(f"Bazel at {bazel_path} does not match SHA256 {versions.bazel_version_sha256}, removing")
-            os.remove(bazel_path)
+            print(f"Bazel at {bazel_path} does not match SHA256 {versions.bazel_version_sha256}")
 
 
     if not os.path.isfile(bazel_path):
